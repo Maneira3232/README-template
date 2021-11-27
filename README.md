ce56a7f4f729d47f4ddc138fb3efe1248bea6098
Maneira3232 <93150997+Maneira3232@users.noreply.github.com>
2021-11-27 13:12:42 -0500

    final touches

--- README.md
+++ README.md
@@ -1,13 +1,57 @@
 # test
 
+## Table of Contents:
 
-    ## Table of Contents
-    [Description](#Description)
-    
-    ## Description: 
+[Description](#Description)
 
-    testing
+[Installation](#Installation)
+
+[Usage](#Usage)
+
+[License](#License)
+
+[Contributors](#Contributors)
+
+--------------------------------------
+
+## Description: 
+
+test
+
+## Installation:
+
+test
+
+## Usage
+
+test
+
+## License
+
+[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache_2.0)
+
+[![License: GPL v3](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://www.gnu.org/licenses/Apache_2.0)
 
 
 
-    
+## Contributors
+
+test
+
+## Tests
+
+test
+
+## Questions
+
+test
+
+## GitHub Username 
+
+test
+
+## Email Info
+
+test
+
+
--- assets/js/generateReadme.js
+++ assets/js/generateReadme.js
@@ -1,17 +1,62 @@
 function generateReadme(data) {
-    return `# ${data.projectTitle}
+return `# ${data.projectTitle}
+
+## Table of Contents:
+
+[Description](#Description)
+
+[Installation](#Installation)
+
+[Usage](#Usage)
+
+[License](#License)
+
+[Contributors](#Contributors)
+
+--------------------------------------
+
+## Description: 
+
+${data.description}
+
+## Installation:
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+## License
+
+[![License](https://img.shields.io/badge/License-${data.license}-blue.svg)](https://opensource.org/licenses/${data.license})
+
+[![License: GPL v3](https://img.shields.io/badge/License-${data.license}-blue.svg)](https://www.gnu.org/licenses/${data.license})
 
 
-    ## Table of Contents
-    [Description](#Description)
-    
-    ## Description: 
 
-    ${data.description}
+## Contributors
+
+${data.contributing}
+
+## Tests
+
+${data.tests}
+
+## Questions
+
+${data.questions}
+
+## GitHub Username 
+
+${data.githubusername}
+
+## Email Info
+
+${data.email}
 
 
-
-    `;
+`;
 }
 
 module.exports = generateReadme
